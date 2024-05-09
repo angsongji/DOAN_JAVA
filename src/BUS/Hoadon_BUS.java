a25711e48a5d3bd3cc0e5356eb1e46af420675e2
PhuongUyen1311 <150026609+PhuongUyen1311@users.noreply.github.com>
2024-05-09 15:44:22 +0700

    Update Hoadon_BUS.java

--- src/BUS/Hoadon_BUS.java
+++ src/BUS/Hoadon_BUS.java
@@ -4,6 +4,8 @@ package BUS;
 import DTO.Hoadon_DTO;
 import DAO.Hoadon_DAO;
 import java.util.ArrayList;
+import DTO.chitietsanpham_DTO;
+import java.sql.SQLException;
 
 public final class Hoadon_BUS {
     public ArrayList<Hoadon_DTO> dshoadon;
@@ -17,7 +19,38 @@ public final class Hoadon_BUS {
         Hoadon_DAO dshd = new Hoadon_DAO();
         dshoadon = dshd.listchucnang();
     }
+
+     public void delete(Hoadon_DTO hd) throws SQLException {
+        Hoadon_DAO hdDAO = new Hoadon_DAO();
+        chitietsanpham_BUS cp = new chitietsanpham_BUS();
+        ChitietHD_BUS cd = new ChitietHD_BUS(hd.getMaHD());
+        
+        ArrayList<chitietsanpham_DTO> list = cd.listtorestore(hd.getMaHD());
+        for(chitietsanpham_DTO ctsp : list){
+           cp.Restore_pro(ctsp);
+        }
+        cd.delete(hd.getMaHD());
+        hdDAO.delete(hd.getMaHD());   
+    }
     
+    public ArrayList<Hoadon_DTO> search(ArrayList<String> data_filter) {
+    ArrayList<Hoadon_DTO> re = new ArrayList<>();
+    for (String i : data_filter) {
+        for (Hoadon_DTO j : dshoadon) {
+            String idHD = j.getMaHD();
+            if (idHD.equalsIgnoreCase(i))
+                re.add(j);
+            int idKH = j.getMaKH();
+            if (idKH == Integer.parseInt(i))
+                re.add(j);
+            String idNV = j.getMaNV();
+            if (idNV.equalsIgnoreCase(i))
+                re.add(j);
+        }
+    }
+    return re;
+}
+
      public static void main(String[] args) {
         Hoadon_BUS cthd = new Hoadon_BUS();
         cthd.list();
